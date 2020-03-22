88055907bb16ae5733a6ddc82cc3cecaed0807c1
Wafell <61581302+Wafell@users.noreply.github.com>
2020-03-23 02:01:04 +0300

    Update task4.pas

--- lw2/task4.pas
+++ lw2/task4.pas
@@ -2,20 +2,32 @@ PROGRAM WorkWithQueryString(INPUT, OUTPUT);
 USES
   DOS;
 FUNCTION GetQueryStringParameter(Key: STRING): STRING;
+VAR
+  PStart, PLenght, PEnd: BYTE;
 BEGIN
-  IF GetEnv('QUERY_STRING') = First Name
+  PStart :=  POS(Key, GetEnv('QUERY_STRING')) + LENGTH(Key) + LENGTH('=');
+  IF PStart <> 0
   THEN
-    WRITELN('First Name: ', GetQueryStringParameter('first_name'));
-  ELSE
-    IF GetEnv('QUERY_STRING') = Last Name 
-    THEN
-      WRITELN('Last Name: ', GetQueryStringParameter('last_name'));
-    ELSE
-      IF GetEnv('QUERY_STRING') = Age
+    BEGIN
+      IF COPY(GetEnv('QUERY_STRING'), PStart, 1) <> '='
       THEN
-        WRITELN('Age: ', GetQueryStringParameter('age')); 
+        BEGIN
+          PEnd := PStart;
+          WHILE (PEnd <> LENGTH(GetEnv('QUERY_STRING'))) AND (COPY(GetEnv('QUERY_STRING'), PEnd, 1) <> '&')
+          DO
+            PEnd := PEnd + 1;
+          IF COPY(GetEnv('QUERY_STRING'), PEnd, 1) = '&'
+          THEN
+            PLenght := PEnd - PStart
+          ELSE
+            PLenght := PEnd - PStart + 1;
+          GetQueryStringParameter := COPY(GetEnv('QUERY_STRING'), PStart, PLenght)
+        END
+    END
 END;
 BEGIN {WorkWithQueryString}
+  WRITELN('Content-Type: text/plain');
+  WRITELN;
   WRITELN('First Name: ', GetQueryStringParameter('first_name'));
   WRITELN('Last Name: ', GetQueryStringParameter('last_name'));
   WRITELN('Age: ', GetQueryStringParameter('age'))
