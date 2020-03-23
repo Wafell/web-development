953ad5075f27ed2f45773fb029aedb4519ff3df9
Wafell <61581302+Wafell@users.noreply.github.com>
2020-03-23 03:08:14 +0300

    Update task5.php

--- lw3/task5.php
+++ lw3/task5.php
@@ -2,12 +2,15 @@
   if ($_GET['email'] <> null)
   {
     $anketa = fopen('data/'.$_GET['email'].'.txt', 'r');
-    echo 'First name: '.substr(fget($anketa), strlen(;
-    echo 'Last name: '.fget($anketa);
-    echo 'Email: '.fget($anketa);
-    echo 'Age: '.fget($anketa);
+    $data = fgets($anketa);
+    echo substr($data, 0, strlen($data)).'<br/>';
+    $data = fgets($anketa);
+    echo substr($data, 0, strlen($data)).'<br/>';
+    $data = fgets($anketa);
+    echo substr($data, 0, strlen($data)).'<br/>';
+    $data = fgets($anketa);
+    echo substr($data, 0, strlen($data)).'<br/>';
     fclose($anketa);
-    //echo 'done!!';
   }
   else
   {
