5b6426fc4073745df100ccb24835b62ca89a14be
Wafell <61581302+Wafell@users.noreply.github.com>
2020-03-25 20:01:44 +0300

    Update task5.php

--- lw3/task5.php
+++ lw3/task5.php
@@ -1,19 +1,26 @@
 <?php
-  if ($_GET['email'] <> null)
-  {
-    $anketa = fopen('data/'.$_GET['email'].'.txt', 'r');
-    $data = fgets($anketa);
-    echo substr($data, 0, strlen($data)).'<br/>';
-    $data = fgets($anketa);
-    echo substr($data, 0, strlen($data)).'<br/>';
-    $data = fgets($anketa);
-    echo substr($data, 0, strlen($data)).'<br/>';
-    $data = fgets($anketa);
-    echo substr($data, 0, strlen($data)).'<br/>';
-    fclose($anketa);
-  }
-  else
-  {
+if ($_GET['email'] <> null)
+{
+    if (file_exists('data/'.$_GET['email'].'.txt'))
+    {
+        $anketa = fopen('data/'.$_GET['email'].'.txt', 'r');
+        $data = fgets($anketa);
+        echo substr($data, 0, strlen($data)).'<br/>';
+        $data = fgets($anketa);
+        echo substr($data, 0, strlen($data)).'<br/>';
+        $data = fgets($anketa);
+        echo substr($data, 0, strlen($data)).'<br/>';
+        $data = fgets($anketa);
+        echo substr($data, 0, strlen($data)).'<br/>';
+        fclose($anketa);
+    }
+    else
+    {
+        echo 'no file';
+    }
+}
+else
+{
     echo 'no email';
-  }
+}
 ?>
