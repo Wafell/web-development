5042a3b68d676d60018150d33aca94839f3e8afc
Wafell <61581302+Wafell@users.noreply.github.com>
2020-04-04 01:27:40 +0300

    Update task1.js

--- lw7/js/task1.js
+++ lw7/js/task1.js
@@ -1,39 +1,45 @@
 function isPrimeNumber(n) {
-	let isPrime
-	isPrime = true
-	if (typeof n == 'number') {
-        for (j = 2; j < n; j++) {
-            if (i % j == 0) { 	
-        	    isPrime = false
-        	    break
-	        }	    
-        }
-        if (isPrime) {
-        	console.log(n + ' is prime number') 	
-        } else{
-            console.log(n + ' is not prime number') 
-        }
-	} else {
-		if (typeof n == 'object') {
-			for (let i = 0; i < n.length; i++) {
-				if (typeof n[i] == 'number'){
-					for (let j = 2; j < n[i]; j++) {
-                        if (n[i] % j == 0) {
-                            isPrime = false
-                            break
-                        }
-                    }
-                    if (isPrime) {
-                    	console.log(n[i] + ' is prime number')
-                    } else {
-                    	console.log(n[i] + ' is not prime number')
-                    }
-                } else {
-                	console.log(n[i] + ' is an invalid data type')
-                }
-            }
-        } else {
-        	console.log(n + ' is an invalid data type')
-        }                                
+  let isPrime
+    
+  if (typeof n == 'number') {
+    for (j = 2; j < n; j++) {
+      isPrime = true    
+      if (n % j == 0) {     
+        isPrime = false
+        break
+      }        
     }
+    if (isPrime) {
+      console.log(n + ' is prime number')     
+    } else{
+      console.log(n + ' is not prime number') 
+    }
+  } else {
+    if (typeof n == 'object') {
+      if (n.length == 0) {
+        console.log('array empty')
+      } else {          
+        for (let i = 0; i < n.length; i++) {
+          isPrime = true
+          if (typeof n[i] == 'number'){
+            for (let j = 2; j < n[i]; j++) {
+              if (n[i] % j == 0) {
+                isPrime = false
+                break
+              }
+            }
+            if (isPrime) {
+              console.log(n[i] + ' is prime number')
+            } else {
+              console.log(n[i] + ' is not prime number')
+            }
+          } else {
+            console.log(n[i] + ' is an invalid data type')
+          }
+        }
+      } 
+    } else {
+      console.log(n + ' is an invalid data type')
+    }                                
+  }
 }
