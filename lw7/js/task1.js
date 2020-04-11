0567c6dc7ba0edf93d03eaf01ed5773ca6cc619e
Wafell <61581302+Wafell@users.noreply.github.com>
2020-04-11 19:38:35 +0300

    Update task1.js

--- lw7/js/task1.js
+++ lw7/js/task1.js
@@ -1,45 +1,45 @@
 function isPrimeNumber(n) {
-  let isPrime
+  let isPrime;
     
   if (typeof n == 'number') {
     for (j = 2; j < n; j++) {
-      isPrime = true    
+      isPrime = true;    
       if (n % j == 0) {     
-        isPrime = false
-        break
+        isPrime = false;
+        break;
       }        
     }
     if (isPrime) {
-      console.log(n + ' is prime number')     
+      console.log(n + ' is prime number');     
     } else{
-      console.log(n + ' is not prime number') 
+      console.log(n + ' is not prime number'); 
     }
   } else {
     if (typeof n == 'object') {
       if (n.length == 0) {
-        console.log('array empty')
+        console.log('array empty');
       } else {          
         for (let i = 0; i < n.length; i++) {
-          isPrime = true
+          isPrime = true;
           if (typeof n[i] == 'number'){
             for (let j = 2; j < n[i]; j++) {
               if (n[i] % j == 0) {
-                isPrime = false
-                break
+                isPrime = false;
+                break;
               }
             }
             if (isPrime) {
-              console.log(n[i] + ' is prime number')
+              console.log(n[i] + ' is prime number');
             } else {
-              console.log(n[i] + ' is not prime number')
+              console.log(n[i] + ' is not prime number');
             }
           } else {
-            console.log(n[i] + ' is an invalid data type')
+            console.log(n[i] + ' is an invalid data type');
           }
         }
       } 
     } else {
-      console.log(n + ' is an invalid data type')
+      console.log(n + ' is an invalid data type');
     }                                
   }
 }
