e5d9de9a0f50f160cca1f90203838978bd2bb380
Wafell <61581302+Wafell@users.noreply.github.com>
2020-05-27 02:48:20 +0300

    Update 201.pas

--- LW 20/201.pas
+++ LW 20/201.pas
@@ -1,50 +1,53 @@
 PROGRAM XPrint(INPUT, OUTPUT);
 CONST
   StartMatrix = 1;
-  MaxCount = 5;
-  FinalMatrix = 25;
+  LetterSize = 5;
+  SymbolsInLetter = LetterSize * LetterSize;
   PrintX = 'X';
   Space = ' ';
 TYPE
-  MatrixType = SET OF StartMatrix .. FinalMatrix;   
+  MatrixType = SET OF StartMatrix .. SymbolsInLetter;   
 VAR
   Ch: CHAR;
-  I, J, Count: INTEGER;
-FUNCTION LetterMatrix(VAR Ch: CHAR): MatrixType;
+  I, J, Count, StartCount, FinalCount: INTEGER;
+FUNCTION GetLetterMatrix(VAR Ch: CHAR): MatrixType;
 BEGIN 
   CASE Ch OF
-    'A': LetterMatrix := [3, 7, 9, 11, 15, 16, 17, 18, 19, 20, 21, 25];
-    'M': LetterMatrix := [1, 5, 6, 7, 9, 10, 11, 13, 15, 16, 20, 21, 25];
-    'F': LetterMatrix := [1, 2, 3, 4, 5, 6, 11, 12, 13, 14, 16, 21];
-    'I': LetterMatrix := [1, 2, 3, 4, 5, 8, 13, 18, 21, 22, 23, 24, 25];
-    'R': LetterMatrix := [1, 2, 3, 6, 9, 11, 12, 13, 16, 18, 21, 24]
+    'A': GetLetterMatrix := [3, 7, 9, 11, 15, 16, 17, 18, 19, 20, 21, 25];
+    'M': GetLetterMatrix := [1, 5, 6, 7, 9, 10, 11, 13, 15, 16, 20, 21, 25];
+    'F': GetLetterMatrix := [1, 2, 3, 4, 5, 6, 11, 12, 13, 14, 16, 21];
+    'I': GetLetterMatrix := [1, 2, 3, 4, 5, 8, 13, 18, 21, 22, 23, 24, 25];
+    'R': GetLetterMatrix := [1, 2, 3, 6, 9, 11, 12, 13, 16, 18, 21, 24]
   ELSE 
-    LetterMatrix := []     
+    GetLetterMatrix := []     
   END
 END;   
 PROCEDURE Print (VAR FOut: TEXT; Ch: CHAR);
 VAR
   Matrix: MatrixType; 
 BEGIN 
-  Count := StartMatrix;
-  Matrix := LetterMatrix(Ch);
+  Count := 0;
+  Matrix := GetLetterMatrix(Ch);
   IF Matrix = []
   THEN
     WRITELN('PLEASE, ENTER ANOTHER LETTER')
   ELSE
     BEGIN  
-      FOR I := StartMatrix TO MaxCount
+      FOR I := StartMatrix TO LetterSize
       DO
         BEGIN
-          FOR J := Count TO (Count + 4)
+          StartCount := Count * 5 + 1;
+          FinalCount := StartCount + 4; 
+          FOR J := StartCount TO FinalCount
           DO  
             BEGIN
               IF J IN Matrix
               THEN
                 WRITE(FOut, PrintX)
               ELSE
-                WRITE(FOut, Space)
-            END;
+                WRITE(FOut, Space);                
+            END; 
+          Count := Count + 1;  
           WRITELN  
         END
     END    
