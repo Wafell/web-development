69a8a6d78360a707dc492c3dc56660b7709458ab
Wafell <61581302+Wafell@users.noreply.github.com>
2020-06-12 04:46:37 +0300

    Update 173.pas

--- lw17/173.pas
+++ lw17/173.pas
@@ -1,105 +1,89 @@
 PROGRAM Stat(INPUT, OUTPUT);
+CONST
+  MaxInt = 32767;
 VAR
-  I, N, CountNum, Sum, Average, Min, Max: INTEGER;
-  Ch, Over: CHAR;
-PROCEDURE ReadNumber(VAR F: TEXT; VAR N: INTEGER);       
-BEGIN  
-  IF Ch = '0' THEN N := 0; 
-  IF Ch = '1' THEN N := 1; 
-  IF Ch = '2' THEN N := 2; 
-  IF Ch = '3' THEN N := 3; 
-  IF Ch = '4' THEN N := 4; 
-  IF Ch = '5' THEN N := 5; 
-  IF Ch = '6' THEN N := 6; 
-  IF Ch = '7' THEN N := 7; 
-  IF Ch = '8' THEN N := 8; 
-  IF Ch = '9' THEN N := 9;
-  IF I < 3276
+  I, Min, Max, Sum, CountNum, Average: INTEGER;
+  Ch: CHAR;
+  Error: BOOLEAN;
+PROCEDURE ReadDigit(VAR F: TEXT; VAR N: INTEGER);       
+BEGIN 
+  N := -1;
+  IF NOT EOLN(F)
   THEN
-    I := I * 10 + N 
-  ELSE
-    IF I = 3276
-    THEN
-      BEGIN
-        IF N <= 7
-        THEN
-          I := I * 10 + N
-        ELSE
-          BEGIN
-            I := -1;
-            Over := '2'
-          END   
-      END 
-    ELSE
-      IF I > 3276
-      THEN
-        BEGIN
-          I := -1;
-          Over := '2'
-        END                                              
-END;  
+    BEGIN  
+      READ(F, Ch);
+      IF Ch = '0' THEN N := 0; 
+      IF Ch = '1' THEN N := 1; 
+      IF Ch = '2' THEN N := 2; 
+      IF Ch = '3' THEN N := 3; 
+      IF Ch = '4' THEN N := 4; 
+      IF Ch = '5' THEN N := 5; 
+      IF Ch = '6' THEN N := 6; 
+      IF Ch = '7' THEN N := 7; 
+      IF Ch = '8' THEN N := 8; 
+      IF Ch = '9' THEN N := 9   
+    END   
+END; 
+PROCEDURE ReadNumber(VAR F: TEXT; VAR I: INTEGER);
+VAR
+  N: INTEGER;
 BEGIN
-  Sum := 0;
-  Min := 32767;
-  Max := 0;
   I := 0;
-  Over := '1';
+  ReadDigit(INPUT, N);
+  WHILE (I <> -1) AND (N <> -1)  
+  DO
+    BEGIN
+      IF (N <= (MaxInt -  I * 10))
+      THEN
+        I := I * 10 + N
+      ELSE
+        I := -1;
+      ReadDigit(INPUT, N)  
+    END         
+END; 
+BEGIN
+  Min := MaxInt;
+  Max := 0;
+  CountNum := 0;
+  Sum := 0;
+  Error := FALSE;
   IF EOLN
   THEN
     WRITELN
   ELSE
     BEGIN
-      WHILE NOT EOLN
-      DO 
+      WHILE (NOT EOLN) AND (NOT Error)
+      DO
         BEGIN
-          READ(Ch);
-          WHILE (Ch <> ' ') 
-          DO 
-            BEGIN 
-              I := 0;  
-              WHILE (Ch >= '0') AND (Ch <= '9') AND (I <> -1)
-              DO
-                BEGIN
-                  ReadNumber(INPUT, N);
-                  IF NOT EOLN
-                  THEN
-                    READ(Ch)
-                  ELSE
-                    Ch := ' '
-                END;
-                IF (I <= 32767) AND (I <> -1)
-                THEN
-                  BEGIN
-                    IF Min >= I
-                    THEN
-                      Min := I;
-                    IF Max <= I
-                    THEN
-                      Max := I
-                  END
-                ELSE
-                  WRITELN('Переполнение');          
-                Sum := Sum + I
-            END;
-            IF Sum > 32767
-            THEN
-              BEGIN 
-                Over := '2';
-                WRITELN('Переполнение')
-              END;           
-          CountNum := CountNum + 1    
-        END
-    END;
-  IF Over = '1'
-  THEN
-    BEGIN
-      Average := (Sum * 10) DIV CountNum;
-      IF Average MOD 10 >= 5
+          ReadNumber(INPUT, I);
+          IF (I <= (MaxInt - Sum)) AND (I <> -1)
+          THEN
+            BEGIN
+              CountNum := CountNum + 1;
+              Sum := Sum + I;
+              IF I < Min
+              THEN
+                Min := I;
+              IF I > Max
+              THEN
+                Max := I   
+            END     
+          ELSE
+            Error := TRUE            
+        END;
+      IF NOT Error
       THEN
-        WRITELN(Average DIV 10 + 1)
+        BEGIN  
+          WRITELN('Min: ', Min);
+          WRITELN('Max: ', Max);
+          Average := (Sum * 10) DIV CountNum;
+          IF Average MOD 10 >= 5
+          THEN
+            WRITELN('Average: ', Average DIV 10 + 1)
+          ELSE
+            WRITELN('Average: ', Average DIV 10)  
+        END 
       ELSE
-        WRITELN(Average DIV 10);
-      WRITELN('Min: ', Min);
-      WRITELN('Max: ', Max)
-    END    
-END.
+        WRITELN('OVERFLOW')   
+    END     
+END.         
